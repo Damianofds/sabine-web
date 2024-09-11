5017f3573fcdb3fec11970c1d32dfee19411a076
Damianofds <damianofds@gmail.com>
2024-09-11 19:13:34 +0200

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,34 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Roboto } from 'next/font/google';
+import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
-    weight: ['400','700'],
-    subsets: ['latin'],
+    weight: ["400", "700"],
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Sabine.bot - your first personal assistant",
-  description: "Sabine.bot is your personal assistant and it does paperwork and scheduling for you",
+    title: "Sabine.bot - your first personal assistant",
+    description:
+        "Sabine.bot is your personal assistant and it does paperwork and scheduling for you",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={roboto.className}>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon.png"
+                />
+            </head>
+            <body className={roboto.className}>{children}</body>
+        </html>
+    );
 }
